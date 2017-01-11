4dd1dbcfe7689c4e0cdc633f31a7e5aee4ec9770
Sergey Chikuyonok <serge.che@gmail.com>
2017-01-11 11:55:07 +0300

    Fixed root module

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import html from './html';
+import { expand } from './html';
 
 // XXX will add CSS support later
 
 export default function(abbr, options) {
-    return html(abbr, options);
+    return expand(abbr, options);
 }
